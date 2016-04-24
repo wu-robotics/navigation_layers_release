11f58201a782d4bd7c26b2000b039a7e8f6725c7
David Lu <davidvlu@gmail.com>
2016-04-24 15:38:11 -0400

    Rebase from 'debian/indigo/range_sensor_layer'

--- include/range_sensor_layer/range_sensor_layer.h
+++ include/range_sensor_layer/range_sensor_layer.h
@@ -3,9 +3,8 @@
 #include <ros/ros.h>
 #include <costmap_2d/costmap_layer.h>
 #include <costmap_2d/layered_costmap.h>
-#include <costmap_2d/GenericPluginConfig.h>
 #include <sensor_msgs/Range.h>
-//#include <range_sensor_layer/RangeSensorLayerConfig.h>
+#include <range_sensor_layer/RangeSensorLayerConfig.h>
 #include <dynamic_reconfigure/server.h>
 
 namespace range_sensor_layer
@@ -14,16 +13,31 @@ namespace range_sensor_layer
 class RangeSensorLayer : public costmap_2d::CostmapLayer
 {
 public:
+  enum InputSensorType
+  {
+    VARIABLE,
+    FIXED,
+    ALL
+  };
+
   RangeSensorLayer();
 
   virtual void onInitialize();
   virtual void updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x, double* min_y, double* max_x,
                              double* max_y);
   virtual void updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j);
+  virtual void reset();
 
 private:
-  void reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level);
-  void incomingRange(const sensor_msgs::RangeConstPtr& range);
+  void reconfigureCB(range_sensor_layer::RangeSensorLayerConfig &config, uint32_t level);
+
+  void bufferIncomingRangeMsg(const sensor_msgs::RangeConstPtr& range_message);
+  void processRangeMsg(sensor_msgs::Range& range_message);
+  void processFixedRangeMsg(sensor_msgs::Range& range_message);
+  void processVariableRangeMsg(sensor_msgs::Range& range_message);
+
+  void updateCostmap();
+  void updateCostmap(sensor_msgs::Range& range_message, bool clear_sensor_cone);
 
   double gamma(double theta);
   double delta(double phi);
@@ -34,6 +48,10 @@ private:
 
   double to_prob(unsigned char c){ return double(c)/costmap_2d::LETHAL_OBSTACLE; }
   unsigned char to_cost(double p){ return (unsigned char)(p*costmap_2d::LETHAL_OBSTACLE); }
+
+  boost::function<void (sensor_msgs::Range& range_message)> processRangeMessageFunc_;
+  boost::mutex range_message_mutex_;
+  std::list<sensor_msgs::Range> range_msgs_buffer_;
 
   double max_angle_, phi_v_;
   std::string global_frame_;
@@ -47,7 +65,7 @@ private:
   std::vector<ros::Subscriber> range_subs_;
   double min_x_, min_y_, max_x_, max_y_;
 
-  dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig> *dsrv_;
+  dynamic_reconfigure::Server<range_sensor_layer::RangeSensorLayerConfig> *dsrv_;
 };
 }
 #endif
